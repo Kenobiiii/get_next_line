e75e46521835b30bb75a0651eb73c2fd7cbebc92
Pablo Romero Pons-Fuster <pablo.romeroponsfuster@gmail.com>
2024-02-14 18:01:53 +0100

    Cambios menores

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,36 @@
 
 #include "get_next_line.h"
 
+
+char	ft_free(char *buffer, char buf)
+{
+	char	*tmp;
+	temp = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (temp);
+}
+
+char	ft_next(char *buffer)
+{
+	
+}
+
+char	ft_line(char *buffer)
+{}
+
+char	ft_*read(int fd, char *ret)
+{
+	char	buf;
+	int		ret;
+
+	ret = read(fd, &buf, 1);
+	if (ret == -1)
+		return (NULL);
+	if (ret == 0)
+		return (buffer);
+	buffer = ft_free(buffer, buf);
+	return (buffer);
+}
 char	*get_next_line(int fd)
 {
 	if (!fd)
