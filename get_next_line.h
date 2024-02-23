f1e8fb94fffca168a8a9d39fd715f07e3b553ab2
paromero <paromeropons@hotmail.com>
2024-02-23 09:38:06 +0100

    añadidas librerias y funciones en header
    se han añadido las librerias para que compile y las funciones
    pertinentes al header

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,30 @@
 /*   By: paromero <paromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:42:11 by paromero          #+#    #+#             */
-/*   Updated: 2024/02/01 11:02:11 by paromero         ###   ########.fr       */
+/*   Updated: 2024/02/23 09:27:28 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <ctype.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdarg.h>
+# include <stdarg.h>
+# include <limits.h>
+
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_line(char *str);
+char	*ft_read(int fd, char *str);
+char	*ft_save(char	*str);
 
 size_t	ft_strlen(const char *str);
 
