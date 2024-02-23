57329726c9083fc475fbac739e41e693ee5e7777
paromero <paromeropons@hotmail.com>
2024-02-23 10:50:26 +0100

    Movido ft_read y añadida comprobación de str

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,35 @@
 /*   By: paromero <paromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:40:22 by paromero          #+#    #+#             */
-/*   Updated: 2024/02/23 09:36:06 by paromero         ###   ########.fr       */
+/*   Updated: 2024/02/23 10:48:43 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_read(int fd, char *str)
+{
+	char	*buff;
+	int		byt;
+
+	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	byt = 1;
+	while ((!str || !ft_strchr(str, '\n')) && byt != 0)
+	{
+		byt = read(fd, buff, BUFFER_SIZE);
+		if (byt == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[byt] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return (str);
+}
 
 char	*ft_line(char *str)
 {
@@ -47,13 +71,13 @@ char	*ft_save(char	*str)
 	char	*save;
 
 	i = 0;
-	while (str[i] != '\n' && str[i])
-		i++;
-	if (!str[i])
+	if (!str)
 	{
 		free(str);
 		return (NULL);
 	}
+	while (str[i] != '\n' && str[i])
+		i++;
 	save = (char *)malloc((ft_strlen(str) - i + 1) * sizeof(char));
 	if (!save)
 		return (NULL);
@@ -66,36 +90,11 @@ char	*ft_save(char	*str)
 	return (save);
 }
 
-char	*ft_read(int fd, char *str)
-{
-	char	*buff;
-	int		byt;
-
-	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
-		return (NULL);
-	byt = 1;
-	while (!ft_strchr(str, '\n') && byt != 0)
-	{
-		byt = read(fd, buff, BUFFER_SIZE);
-		if (byt == -1)
-		{
-			free(buff);
-			return (NULL);
-		}
-		buff[byt] = '\0';
-		str = ft_strjoin(str, buff);
-	}
-	free(buff);
-	return (str);
-}
-
 char	*get_next_line(int fd)
 {
 	static char		*str;
 	char			*linea;
 
-	str = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
 	{
 		free(str);
