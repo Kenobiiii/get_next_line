05ae97564747fc46b9990c6309e11a4f608be12f
Pablo Romero Pons-Fuster <pablo.romeroponsfuster@gmail.com>
2024-03-05 17:18:29 +0100

    revertido cambio en ft_read

--- get_next_line.c
+++ get_next_line.c
@@ -24,7 +24,7 @@ char	*ft_read(int fd, char *str)
 	while ((!ft_strchr(str, '\n')) && byt != 0)
 	{
 		byt = read(fd, buff, BUFFER_SIZE);
-		if (byt <= 0)
+		if (byt == -1)
 		{
 			free(buff);
 			return (NULL);
